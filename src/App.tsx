192cc39762ba79fc76005d60e2c54a1ec7d5a3ae
kane-droid <kaneheath96@gmail.com>
2023-10-10 22:39:42 -0500

    branding

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
 
+        <img src={'Army-logos.svg'} className="logo" alt="Army logo" />
+
+
+        <img src={'ASWF-logo.svg'} className="logo react" alt="ASWF logo" />
+
+      </div>
+      <h1>Army + Software</h1>
+      <div className="card">
         <AddCounter/>
         <MinusCounter/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
